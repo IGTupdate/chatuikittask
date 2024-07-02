13dbb04b2fb2c0eabc7e907edfa9bdc98da4b6e6
IGTupdate <update@igtechso.com>
2024-07-02 17:53:00 +0530

    change image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,19 +5,22 @@ const Header = () => {
         <div className="flex justify-between items-center flex-wrap px-4 py-5">
             <div className="flex justify-end -space-x-1">
                 <div className="avatar_group">
-                    <img src="/images/drago-icon.png" alt="" className="w-full h-full object-cover" />
+                    <img src="/images/avatar1.png" alt="" className="w-full h-full object-cover" />
                 </div>
                 <div className="avatar_group">
-                    <img src="/images/drago-icon.png" alt="" className="w-full h-full object-cover" />
+                    <img src="/images/avatar2.png" alt="" className="w-full h-full object-cover" />
                 </div>
                 <div className="avatar_group">
-                    <img src="/images/drago-icon.png" alt="" className="w-full h-full object-cover" />
+                    <img src="/images/avatar3.png" alt="" className="w-full h-full object-cover" />
+                </div>
+                <div className="avatar_group">
+                    <img src="/images/avatar4.png" alt="" className="w-full h-full object-cover" />
                 </div>
             </div>
             <div className="text-center">
-                <div className="flex items-center gap-x-2">
-                    <img src="/images/drago-icon.png" alt="" className="w-7 h-7" />
-                    <h2 className="font-semibold">Team Unicorns</h2>
+                <div className="flex items-center justify-center mb-1 gap-x-2">
+                    <img src="/images/logo2.png" alt="" className="w-full max-w-32" />
+                    {/* <h2 className="font-semibold">Team Unicorns</h2> */}
                 </div>
                 <p className="text-sm text-black/70">last seen 45 minutes ago</p>
             </div>
